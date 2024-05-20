6c896d1000373282a5c94fbbbe0b36b2f41243a3
Antoni Jankowski <antek.jankowski.jv@gmail.com>
2024-05-20 15:26:57 +0200

    Widgets update

--- src/js/components/Booking.js
+++ src/js/components/Booking.js
@@ -1,5 +1,7 @@
 import { templates, select } from '../settings.js';
 import AmountWidget from './AmountWidget.js';
+import DatePicker from './DatePicker.js';
+import HourPicker from './HourPicker.js';
 
 
 class Booking{
@@ -23,9 +25,9 @@ class Booking{
         thisBooking.dom.peopleAmount = element.querySelector(select.booking.peopleAmount);
         thisBooking.dom.hoursAmount = element.querySelector(select.booking.hoursAmount);
 
-        thisBooking
+        thisBooking.dom.datePicker = element.querySelector(select.widgets.datePicker.wrapper);
+        thisBooking.dom.hourPicker = element.querySelector(select.widgets.hourPicker.wrapper);
 
-        
     }
 
     initWidgets(){
@@ -33,6 +35,9 @@ class Booking{
 
         thisBooking.peopleAmountWidget = new AmountWidget(thisBooking.dom.peopleAmount);
         thisBooking.hoursAmountWidget = new AmountWidget(thisBooking.dom.hoursAmount);
+
+        thisBooking.datePickerWidget = new DatePicker(thisBooking.dom.datePicker);
+        thisBooking.hourPickerWidget = new HourPicker(thisBooking.dom.hourPicker);
         
     }
 
